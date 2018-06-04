9ed7967181dc06202404cdff8ab4d6c159bd6f3f
Grace Ho <rainczech@gmail.com>
2018-06-04 11:50:15 -0500

    setup files and boiler code

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+const exphbs = require("express-handlebars");
+const bodyParser = require ("body-parser");
+
+// serve static content form public directory
+app.use(express.static("public"));
+ app.use(bodyParser.urlencoded({extended:true}));
+//  parse application/json
+app.use(bodyParse.json());
+
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+
+const htmlRoutes = require("");
+const apiRoutes = require("");
+
+app.use(htmlRoutes);
+app.use(apiRoutes);
+
+app.listen(PORT, function (){
+    console.log("listening on PORT " + PORT);
+});
