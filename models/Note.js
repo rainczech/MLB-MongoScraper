52b53ac97fc5798c115e9584ab909c51382b67b0
Grace Ho <rainczech@gmail.com>
2018-07-06 22:28:46 -0500

    note model updated

--- models/Note.js
+++ models/Note.js
@@ -2,11 +2,18 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const NoteSchema = new Schema ({
-    title: String,
-    body: String
+const noteSchema = new Schema ({
+    _headlineId: {
+        type: Schema.Types.ObjectId,
+        ref: "Article"
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    noteBody: String
 });
 
-const Note = mongoose.model("Note", NoteSchema);
+const Note = mongoose.model("Note", noteSchema);
 
 module.exports = Note;
